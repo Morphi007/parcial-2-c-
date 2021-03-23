f410cd49228a772fdea74c72d99f833d59f7f302
Morphi007 <78938079+Morphi007@users.noreply.github.com>
2021-03-23 13:19:34 -0400

    Update ejercicio2.cs

--- ejercicio2.cs
+++ ejercicio2.cs
@@ -1,1 +1,54 @@
-﻿
+using System;
+
+
+
+namespace parcial_ejercicio2
+{
+    class empleado
+    {
+        private string[] nombres;
+        private float[] salario;
+
+        public void Cargar()
+        {
+            nombres = new string[6];
+            salario = new float[6];
+            for (int n = 1; n < nombres.Length; n++)
+            {
+                Console.Write("Ingrese el nombre del empleado:{0} ", n);
+
+                nombres[n] = Console.ReadLine();
+                Console.Write("Ingrese el sueldo:");
+                string carga;
+                carga = Console.ReadLine();
+                salario[n] = float.Parse(carga);
+            }
+        }
+
+        public void MayorSueldo()
+        {
+            float mayor;
+            int pos;
+            mayor = salario[0];
+            pos = 0;
+            for (int i = 1; i < nombres.Length; i++)
+            {
+                if (salario[i] > mayor)
+                {
+                    mayor = salario[i];
+                    pos = i;
+                }
+            }
+            Console.WriteLine("El empleado con sueldo mayor es " + nombres[pos]);
+            Console.WriteLine("Tiene un sueldo:" + mayor);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            empleado empleado = new empleado();
+            empleado.Cargar();
+            empleado.MayorSueldo();
+        }
+    }
+}
